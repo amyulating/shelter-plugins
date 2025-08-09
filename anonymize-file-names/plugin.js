3a3ab3f3a182b6e71972580fbaa795c7f4135911
amyulating <amyulating@users.noreply.github.com>
2025-08-09 02:21:30 +0000

    deploy: d56427590abf8e3e40c5a721ce89c39e966c7d0e

---
+++ anonymize-file-names/plugin.js
@@ -0,0 +1,43 @@
+(function(exports) {
+
+"use strict";
+
+//#region plugins/anonymize-file-names/index.js
+const { flux: { intercept } } = shelter;
+function getRandomArrayMember(arr) {
+	if (arr.length === 0) return null;
+	const randomIndex = Math.floor(Math.random() * arr.length);
+	return arr[randomIndex];
+}
+function createRandomBullshit(length) {
+	let bullshit = "";
+	const bullshitChars = "qwertyuiopasdfghjklzxcvbnm1234567890".split("");
+	for (let i = 0; i <= length; i++) {
+		let temp = getRandomArrayMember(bullshitChars);
+		if (Math.random() > .5) bullshit += temp.toUpperCase();
+else bullshit += temp;
+	}
+	return bullshit;
+}
+unintercept = intercept((dispatch) => {
+	if (dispatch?.type === "UPLOAD_ATTACHMENT_ADD_FILES") {
+		dispatch?.files?.forEach(({ file }) => {
+			if (!file?.name) return;
+			let newFilename = createRandomBullshit(10);
+			if (file.name.includes(".")) {
+				const dotIndex = store.matchDotSeparators ? file.name.lastIndexOf(".") : file.name.indexOf(".");
+				newFilename += file.name.slice(dotIndex);
+			}
+			Object.defineProperty(file, "name", { value: newFilename });
+		});
+		return dispatch;
+	}
+});
+function onUnload() {
+	unintercept();
+}
+
+//#endregion
+exports.onUnload = onUnload
+return exports;
+})({});
